2f3a0b8431d4eff493b8ce88f1791c47f00ab66f
workneel2111 <pneel9224@gmail.com>
2026-06-11 19:36:39 +0530

    .gitignore

---
+++ app.py
@@ -0,0 +1,48 @@
+from flask import Flask, render_template, request, redirect, url_for
+from pymongo import MongoClient
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+print("Current Directory:", os.getcwd())
+
+app = Flask(__name__)
+
+client = os.getenv("MONGO_URL")
+
+db = client["studentdb"]
+collection = db["students"]
+
+@app.route('/', methods=['GET','POST'])
+def form():
+
+    if request.method == 'POST':
+
+        try:
+
+            data = {
+                "name": request.form['name'],
+                "email": request.form['email']
+            }
+
+            collection.insert_one(data)
+
+            return redirect(url_for('success'))
+
+        except Exception as e:
+
+            return render_template(
+                'form.html',
+                error=str(e)
+            )
+
+    return render_template('form.html')
+
+@app.route('/success')
+def success():
+
+    return render_template('success.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
